d8693e41dca93350e3090047108a861c93dc2540
ritchs <ricardohc03@gmail.com>
2023-02-22 20:32:16 -0600

    senEmail

--- src/components/Hooks/UseSend.js
+++ src/components/Hooks/UseSend.js
@@ -2,7 +2,39 @@ import axios from "axios";
 export default () => {
 	const sendMail = (values) => {
 		console.log("values", values)
-	/* 	const options = {
+		/*var data = JSON.stringify({
+			"collection": "users",
+			"database": "test",
+			"dataSource": "agency",
+			"document": {
+				"name": values.name,
+				"phone": values.name,
+				"email": values.email
+			  }
+		});
+					
+		var config = {
+			method: 'post',
+			url: 'https://us-east-2.aws.data.mongodb-api.com/app/data-hfpzc/endpoint/insertclient',
+			headers: {
+			  'Content-Type': 'application/json',
+			  'Access-Control-Request-Headers': '*',
+			  'api-key': 'E8RbbiMbUXynlkN7MwFdRTNb9zaZnEpEHQMMCO6YPENzd9njAfmlhWKKuA71O41v',
+			  'Accept': 'application/ejson'
+			},
+			data: data
+		};
+					
+		axios(config)
+			.then(function (response) {
+				console.log(JSON.stringify(response.data));
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+		
+
+	 	const options = {
 			method: 'Post',
 			url: "http://localhost:4000/users",
 			body: {
