c7584d6212ab3287341526018ef4969e60a31e3d
Benjamin Glez <ibgm03@gmail.com>
2023-02-23 08:41:03 -0600

    Updates forms

--- src/components/Grupo.jsx
+++ src/components/Grupo.jsx
@@ -3,17 +3,19 @@ import React from 'react'
 export const Grupo = () => {
     return (
         <div className='bg-portada2 bg-cover h-screen'>
-            <div className='grid grid-cols-4 xl:pt-16 2xl:pt-24'>
+            <div className='grid grid-cols-4 lg:pt-16 2xl:pt-24 font-fontMonserrat'>
                 <div className='col-span-1'>
-                    <img src="/images/logo.png" alt="" className='w-40 mx-10' />
+                    <a href="/">
+                        <img src="/images/logo.png" alt="" className='lg:w-36 2xl:w-40 mx-10' />
+                    </a>
                 </div>
                 <div className='col-span-2 text-white'>
-                    <div className='xl:text-8xl 2xl:text-9xl font-bold mb-5'>¡Felicidades!</div>
-                    <div className='xl:text-4xl 2xl:text-5xl'>Estas a un paso de tomar <strong>ACCIÓN y</strong></div>
-                    <div className='xl:text-4xl 2xl:text-5xl mb-14'><strong>emprender en tu vida</strong></div>
-                    <div className='xl:text-4xl 2xl:text-5xl'>Sólo falta unirte a nuestro grupo de</div>
-                    <div className='xl:text-4xl 2xl:text-5xl'><strong>WhatsApp</strong> donde te mantendremos</div>
-                    <div className='xl:text-4xl 2xl:text-5xl mb-14'>informada.</div>
+                    <div className='lg:text-7xl 2xl:text-8xl font-bold mb-5'>¡Felicidades!</div>
+                    <div className='lg:text-3xl 2xl:text-4xl'>Estas a un paso de tomar <strong>ACCIÓN y</strong></div>
+                    <div className='lg:text-3xl 2xl:text-4xl mb-14'><strong>emprender en tu vida</strong></div>
+                    <div className='lg:text-3xl 2xl:text-4xl'>Sólo falta unirte a nuestro grupo de</div>
+                    <div className='lg:text-3xl 2xl:text-4xl'><strong>WhatsApp</strong> donde te mantendremos</div>
+                    <div className='lg:text-3xl 2xl:text-4xl mb-14'>informada.</div>
                     <a href="" className='bg-[#25CB4D] rounded-md px-24 p-2'>UNIRME AL GRUPO</a>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        'fontMonserrat': ['Montserrat', 'sans-serif']
+      },
       backgroundImage: {
         portada: "url('/public/images/back1.png')",
         portada2: "url('/public/images/back2.png')",
