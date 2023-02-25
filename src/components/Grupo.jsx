3895dd4a9da558791be883153b934c45a330fc4b
Benjamin Glez <ibgm03@gmail.com>
2023-02-24 20:33:33 -0600

    update new background

--- src/components/Grupo.jsx
+++ src/components/Grupo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Grupo = () => {
     return (
-        <div className='bg-portada2 bg-cover h-screen'>
+        <div className='bg-ba bg-cover h-screen'>
             <div className='grid grid-cols-4 lg:pt-16 2xl:pt-24 font-fontMonserrat'>
                 <div className='col-span-1'>
                     <a href="/">
@@ -10,13 +10,13 @@ export const Grupo = () => {
                     </a>
                 </div>
                 <div className='col-span-2 text-white'>
-                    <div className='lg:text-7xl 2xl:text-8xl font-bold mb-5'>¡Felicidades!</div>
-                    <div className='lg:text-3xl 2xl:text-4xl'>Estas a un paso de tomar <strong>ACCIÓN y</strong></div>
-                    <div className='lg:text-3xl 2xl:text-4xl mb-14'><strong>emprender en tu vida</strong></div>
-                    <div className='lg:text-3xl 2xl:text-4xl'>Sólo falta unirte a nuestro grupo de</div>
-                    <div className='lg:text-3xl 2xl:text-4xl'><strong>WhatsApp</strong> donde te mantendremos</div>
-                    <div className='lg:text-3xl 2xl:text-4xl mb-14'>informada.</div>
-                    <a href="" className='bg-[#25CB4D] rounded-md px-24 p-2'>UNIRME AL GRUPO</a>
+                    <div className='text-4xl lg:text-7xl 2xl:text-8xl font-bold mb-5'>¡Felicidades!</div>
+                    <div className='text-2xl lg:text-3xl 2xl:text-4xl'>Estas a un paso de tomar <strong>ACCIÓN y</strong></div>
+                    <div className='text-xl lg:text-3xl 2xl:text-4xl mb-8 lg:mb-14'><strong>emprender en tu vida</strong></div>
+                    <div className='text-xl lg:text-3xl 2xl:text-4xl'>Sólo falta unirte a nuestro grupo de</div>
+                    <div className='text-xl lg:text-3xl 2xl:text-4xl'><strong>WhatsApp</strong> donde te mantendremos</div>
+                    <div className='text-xl lg:text-3xl 2xl:text-4xl mb-14'>informada.</div>
+                    <a href="" className='bg-[#25CB4D] rounded-md lg:px-24 p-2'>UNIRME AL GRUPO</a>
                 </div>
             </div>
         </div>
--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Formik, Field, Form } from 'formik';
 import UseSend from '../components/Hooks/UseSend';
+import { redirect } from "react-router-dom";
 export const Registro = () => {
     const { sendMail } = UseSend();
-    
-
-    
     return (
-        <div className='bg-gradient-to-b from-[#3F5D89] to to-[#8A6DC3] bg-contain bg-no-repeat h-[74rem] lg:h-[54rem] 2xl:h-[75rem]'>
-            <div className='grid grid-cols-5 border-black lg:pt-10 2xl:pt-16 font-fontMonserrat'>
-                <div className='col-span-5 lg:col-span-1'>
+        <div className='bg-gradient-to-b from-[#3F5D89] to to-[#8A6DC3] bg-contain bg-no-repeat h-[74rem] lg:h-[50rem] 2xl:h-[75rem]'>
+            <div className='grid grid-cols-5 border-black  2xl:pt-16 font-fontMonserrat'>
+                <div className='col-span-5 lg:col-span-1 lg:pt-10'>
                     <img src="/images/logo.png" alt="" className='w-[5.5rem] lg:w-36 2xl:w-40 mx-[9.5rem] lg:mx-10' /></div>
                 <div className='col-span-5 lg:col-span-2'>
                     <div className='lg:text-7xl 2xl:text-8xl text-2xl lg:text-left text-white font-bold mb-5'>Corona, CA.</div>
@@ -26,17 +24,18 @@ export const Registro = () => {
                                 email: '',
                             }}
                             onSubmit={async (values) => {
-                                if (values.name != '' && values.phone != '' && values.email != '')
-                                {
+                                if (values.name != '' && values.phone != '' && values.email != '') {
                                     const res = sendMail(values)
-                                    if(res.status == 200){
-                                     // aqui tienes que poner la funcion que redirecciona
+                                    if (res.status == 200) {
+                                        redirect("/group");
+
                                     }
+
                                 } else {
                                     const error = {
                                         Nombre: values.name != '' ? 'correcto' : 'Falta el Nombre',
                                         Telefono: values.phone != '' ? 'correcto' : 'Falta el Telefono',
-                                        Email: values.email != '' ? 'correcto':'Falta el Email',
+                                        Email: values.email != '' ? 'correcto' : 'Falta el Email',
                                     }
                                     alert(JSON.stringify(error, null, 2));
                                 }
@@ -44,20 +43,22 @@ export const Registro = () => {
                         >
                             <Form action="">
                                 <label htmlFor="name" className='text-xs'>NOMBRE:</label><br />
-                                <Field  id="name" type="text" name='name' className='w-[23rem] lg:w-[30rem] rounded-md h-10 bg-[#DADEEA] text-black' /><br />
+                                <Field id="name" type="text" name='name' className='w-[23rem] lg:w-[30rem] rounded-md h-10 bg-[#DADEEA] text-black' /><br />
                                 <label htmlFor="phone" className='text-xs'>TELÉFONO</label><br />
                                 <Field id="phone" type="text" name='phone' className='w-[23rem] lg:w-[30rem] rounded-md h-10 bg-[#DADEEA] text-black' /><br />
                                 <label htmlFor="email" className='text-xs'>CORREO ELECTRÓNICO</label><br />
                                 <Field id="email" type="text" name='email' className='w-[23rem] lg:w-[30rem] rounded-md h-10 bg-[#DADEEA] text-black' /><br />
-                               
-                                <button type="submit" className='mt-5 bg-[#25CB4D] rounded-md px-5 p-2'>ENVIAR</button>                                
+
+                                <button type="submit" className='mt-5 bg-[#25CB4D] rounded-md px-5 p-2'> ENVIAR</button>
                             </Form>
                         </Formik>
                     </div>
                 </div>
-                <div className=''></div>
+                <div className='col-span-2'>
+                    <img src="/images/reina.png" alt="" className='lg:w-[33rem]' />
+                </div>
                 <div className=''></div>
             </div>
-        </div>
+        </div >
     )
 }
